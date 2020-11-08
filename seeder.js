79c23dfbd7adc13e358e1035f3745c0a04c07ec1
Amir Askari <theamirsolo@gmail.com>
2020-11-08 12:22:17 +0330

    Add database seeder

---
+++ seeder.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+require('colors')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
+
+// Load models
+const Bootcamp = require('./models/Bootcamp')
+
+// Connect to DB
+mongoose.connect(encodeURI(process.env.MONGO_URI), {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// Read fake JSON data
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+)
+
+// Import data into DB
+async function importData() {
+  try {
+    // Import bootcamps
+    await Bootcamp.create(bootcamps)
+
+    console.log('Data Imported...'.green.inverse)
+
+    // Exit process and close mongoose connection
+    mongoose.connection.close()
+    process.exit(0)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// Destory Datat in DB
+async function destroyData() {
+  try {
+    // Delete all bootcamps
+    await Bootcamp.deleteMany()
+
+    console.log('Data Destroyed...'.red.inverse)
+
+    // Exit process and close mongoose connection
+    mongoose.connection.close()
+    process.exit(0)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+if (process.argv[2] === '-import') {
+  importData()
+} else if (process.argv[2] === '-destroy') {
+  destroyData()
+}
